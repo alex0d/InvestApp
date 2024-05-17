dc5b3fb2113f9ef1d70ede62f80c9bda802decb5
Baranova Ekaterina <ebaranova03@gmail.com>
2024-05-17 17:39:15 +0300

    Update ProfileScreen

--- app/src/main/java/ru/alex0d/investapp/screens/profile/ProfileScreen.kt
+++ app/src/main/java/ru/alex0d/investapp/screens/profile/ProfileScreen.kt
@@ -2,19 +2,32 @@ package ru.alex0d.investapp.screens.profile
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.generated.destinations.MainScreenDestination
 import com.ramcosta.composedestinations.generated.navgraphs.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import org.koin.androidx.compose.koinViewModel
+import ru.alex0d.investapp.R
 import ru.alex0d.investapp.utils.MainGraph
 
 @Destination<MainGraph>
@@ -23,24 +36,78 @@ fun ProfileScreen(
     rootNavigator: DestinationsNavigator,
     viewModel: ProfileViewModel = koinViewModel()
 ) {
-    Box(
+    val state = viewModel.state.collectAsState().value
+
+    Column(
         modifier = Modifier
             .background(MaterialTheme.colorScheme.surface)
             .fillMaxSize()
             .statusBarsPadding(),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = {
-            viewModel.logout()
-            rootNavigator.navigate(RootNavGraph) {
-                popUpTo(MainScreenDestination) {
-                    inclusive = true
+        when (state) {
+            is ProfileState.Error -> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = stringResource(R.string.error_occurred),
+                        textAlign = TextAlign.Center,
+                    )
                 }
             }
-        }) {
-            Text(
-                text = "Logout"
-            )
+
+            is ProfileState.Loading -> {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    CircularProgressIndicator()
+                }
+            }
+
+            is ProfileState.Success -> {
+                Icon(
+                    modifier = Modifier
+                        .size(240.dp)
+                        .padding(top = 32.dp, bottom = 16.dp),
+                    painter = painterResource(id = R.drawable.account_circle),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
+                Text(
+                    modifier = Modifier.padding(bottom = 8.dp),
+                    text = state.email,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Text(
+                    text = "${state.firstname} ${state.lastname ?: ""}".trimEnd(),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.headlineMedium
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Button(
+                    modifier = Modifier.padding(bottom = 32.dp),
+                    onClick = {
+                        viewModel.logout()
+                        rootNavigator.navigate(RootNavGraph) {
+                            popUpTo(MainScreenDestination) {
+                                inclusive = true
+                            }
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+                        contentColor = MaterialTheme.colorScheme.onTertiaryContainer
+                    )
+                ) {
+                    Text(
+                        text = stringResource(R.string.logout),
+                        style = MaterialTheme.typography.labelLarge
+                    )
+                }
+            }
         }
     }
 }
--- app/src/main/java/ru/alex0d/investapp/screens/profile/ProfileViewModel.kt
+++ app/src/main/java/ru/alex0d/investapp/screens/profile/ProfileViewModel.kt
@@ -2,6 +2,7 @@ package ru.alex0d.investapp.screens.profile
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import ru.alex0d.investapp.data.repositories.AuthRepository
 
@@ -9,9 +10,36 @@ class ProfileViewModel(
     private val authRepository: AuthRepository
 ) : ViewModel() {
 
+    private val _state = MutableStateFlow<ProfileState>(ProfileState.Loading)
+    val state: MutableStateFlow<ProfileState> = _state
+
+    init {
+        fetchUserDetails()
+    }
+
+    private fun fetchUserDetails() {
+        viewModelScope.launch {
+            val email = authRepository.getUserEmail()
+            val firstname = authRepository.getUserFirstname()
+            val lastname = authRepository.getUserLastname()
+            if (email != null && firstname != null) {
+                _state.value = ProfileState.Success(email, firstname, lastname)
+            } else {
+                _state.value = ProfileState.Error
+            }
+
+        }
+    }
+
     fun logout() {
         viewModelScope.launch {
             authRepository.logout()
         }
     }
+}
+
+sealed class ProfileState {
+    object Loading : ProfileState()
+    data class Success(val email: String, val firstname: String, val lastname: String?): ProfileState()
+    object Error : ProfileState()
 }
