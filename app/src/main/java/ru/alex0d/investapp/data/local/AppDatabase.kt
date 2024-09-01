7d47f94cdcba9732419358120bf5e2aff09492c4
alex0d <alexey.d707@gmail.com>
2024-09-01 15:15:04 +0300

    Add CI workflow for unit tests

--- app/src/main/java/ru/alex0d/investapp/data/local/AppDatabase.kt
+++ app/src/main/java/ru/alex0d/investapp/data/local/AppDatabase.kt
@@ -5,7 +5,11 @@ import androidx.room.RoomDatabase
 import ru.alex0d.investapp.data.local.dao.TarotPredictionDao
 import ru.alex0d.investapp.data.local.models.TarotPredictionDbo
 
-@Database(entities = [TarotPredictionDbo::class], version = 1)
+@Database(
+    entities = [TarotPredictionDbo::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun tarotPredictionDao(): TarotPredictionDao
 }
