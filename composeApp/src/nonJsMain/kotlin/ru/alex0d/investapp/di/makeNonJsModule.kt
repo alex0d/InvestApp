82ebdb446a4c8626dd59e49c2569437c71c30776
alex0d <alexey.d707@gmail.com>
2025-04-12 23:44:45 +0300

    Fix Room Database

--- composeApp/src/nonJsMain/kotlin/ru/alex0d/investapp/di/makeNonJsModule.kt
+++ composeApp/src/nonJsMain/kotlin/ru/alex0d/investapp/di/makeNonJsModule.kt
@@ -3,10 +3,23 @@ package ru.alex0d.investapp.di
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.room.RoomDatabase
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import kotlinx.coroutines.Dispatchers
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import ru.alex0d.investapp.data.local.AppDatabase
 import ru.alex0d.investapp.screens.stock.chart.ChartAdapterFactory
+import ru.alex0d.investapp.utils.IO
+
+private fun getRoomDatabase(
+    builder: RoomDatabase.Builder<AppDatabase>
+): AppDatabase {
+    return builder
+        .fallbackToDestructiveMigrationOnDowngrade(dropAllTables = true)
+        .setDriver(BundledSQLiteDriver())
+        .setQueryCoroutineContext(Dispatchers.IO)
+        .build()
+}
 
 fun makeNonJsModule(
     appDatabaseBuilder: NativeInjectionFactory<RoomDatabase.Builder<AppDatabase>>,
@@ -14,6 +27,7 @@ fun makeNonJsModule(
 ): Module {
     return module {
         single { appDatabaseBuilder() }
+        single { getRoomDatabase(builder = get()) }
         single { dataStore() }
         single { ChartAdapterFactory() }
     }
